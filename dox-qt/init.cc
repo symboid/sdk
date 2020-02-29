f6a6e6502836db35cb1df004ef5ade901eb71e3f
Török Róbert <torok.robert@symboid.com>
2020-02-29 10:27:38 +0100

    fix of dox module init

--- dox-qt/init.cc
+++ dox-qt/init.cc
@@ -1,11 +1,14 @@
 
 #include "sdk/dox-qt/setup.h"
 #include "sdk/dox-qt/init.h"
+#include <qglobal.h>
 
 mod_sdk_dox_qt::mod_sdk_dox_qt()
 {
+    Q_INIT_RESOURCE(sdk_dox_qt);
 }
 
 mod_sdk_dox_qt::~mod_sdk_dox_qt()
 {
+    Q_CLEANUP_RESOURCE(sdk_dox_qt);
 }
