5d79bf5d91a7b24c0feda0c582fde266c8ec4622
Török Róbert <robert.torok@gmail.com>
2020-05-01 13:48:23 +0200

    translations added

---
+++ dox-qt/sdk-dox-qt_hu.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
+<context>
+    <name>DocumentDialog</name>
+    <message>
+        <location filename="DocumentDialog.qml" line="23"/>
+        <source>Saved horoscopes</source>
+        <translation>Mentett horoszkópok</translation>
+    </message>
+    <message>
+        <location filename="DocumentDialog.qml" line="49"/>
+        <source>Current transit</source>
+        <translation>Jelenlegi tranzit</translation>
+    </message>
+    <message>
+        <location filename="DocumentDialog.qml" line="57"/>
+        <source>Recent horoscopes</source>
+        <translation>Legutóbbi horoszkópok</translation>
+    </message>
+</context>
+</TS>
---
+++ uicontrols-qt/sdk-uicontrols-qt_hu.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu_HU">
+<context>
+    <name>TimeCoordBox</name>
+    <message>
+        <location filename="TimeCoordBox.qml" line="29"/>
+        <source>h</source>
+        <translation>ó</translation>
+    </message>
+    <message>
+        <location filename="TimeCoordBox.qml" line="38"/>
+        <source>m</source>
+        <translation>p</translation>
+    </message>
+    <message>
+        <location filename="TimeCoordBox.qml" line="47"/>
+        <source>s</source>
+        <translation>mp</translation>
+    </message>
+</context>
+</TS>
