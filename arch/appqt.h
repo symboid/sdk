ef660aa4c6afcc64f7e57ce964fbe4175078d30e
Török Róbert <robert.torok@gmail.com>
2020-09-04 22:20:53 +0200

    app_qt rebased onto mod_qt

--- arch/appqt.h
+++ arch/appqt.h
@@ -4,6 +4,7 @@
 
 #include "sdk/arch/defs.h"
 #include "sdk/arch/app.h"
+#include "sdk/arch/modqt.h"
 #include "sdk/arch/mainobject.h"
 #include <QGuiApplication>
 #include <QString>
@@ -11,7 +12,7 @@
 arh_ns_begin
 
 template<>
-QGuiApplication* main_object_create(int* _argc, char*** _argv)
+inline QGuiApplication* main_object_create(int* _argc, char*** _argv)
 {
     return new QGuiApplication(*_argc, *_argv);
 }
@@ -22,12 +23,13 @@ struct qt_application
 };
 
 template <class _AppMod>
-struct app_qt : public app<_AppMod>
+struct app_qt : public mod_qt<_AppMod>
 {
     app_qt(int* _argc, char*** _argv)
         : _M_qt_application(_argc, _argv)
     {
     }
+    mod_main _M_mod_main;
     main_object_init<qt_application, int*, char***> _M_qt_application;
 
     int exec() { return _M_qt_application->exec(); }
