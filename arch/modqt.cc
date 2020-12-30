74f3ff6f88e48ffb672f71e8c0d7af4bb753b6e4
Török Róbert <robert.torok@gmail.com>
2020-12-30 21:33:16 +0100

    fix of translation according to en langcode

--- arch/modqt.cc
+++ arch/modqt.cc
@@ -10,11 +10,16 @@ void basic_qt_translation::load(const QString& _mod_name)
 {
     const QString mod_file_name = QString(_mod_name).replace('_','-');
 
-    const QString config_lang_id("hu");
-    QString qm_file_path = config_lang_id.isEmpty() ? "" :
-            QString(":tr/%1_%2.qm").arg(mod_file_name, config_lang_id);
+    static const QLocale system_locale = QLocale::system();
+    static const QString lang_code = system_locale.name().left(2);
+    QString qm_file_path = lang_code.isEmpty() ? "" :
+            QString(":tr/%1_%2.qm").arg(mod_file_name, lang_code);
 
-    if (!_M_qm_file.load(QLocale::system(), mod_file_name, "_", ":/tr") &&
+    if (lang_code == "en")
+    {
+        log_info << "english translation used for module '" << _mod_name << "'";
+    }
+    else if (!_M_qm_file.load(system_locale, mod_file_name, "_", ":/tr") &&
         !_M_qm_file.load(qm_file_path))
     {
         log_error << "translation for module '" << _mod_name << "' cannot be loaded!";
